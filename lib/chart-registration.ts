ea26c5615741a06d72f2820c9c0eee6577d6fcee
Harikrishnanvkph <harikrishnanvk0531@gmail.com>
2025-07-05 17:11:43 +0530

    Initial Commit

---
+++ lib/chart-registration.ts
@@ -0,0 +1,70 @@
+"use client"
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  BarElement,
+  LineElement,
+  PointElement,
+  ArcElement,
+  // Controllers
+  BarController,
+  LineController,
+  PieController,
+  DoughnutController,
+  PolarAreaController,
+  RadarController,
+  Title,
+  Tooltip,
+  Legend,
+  RadialLinearScale,
+  Filler,
+  ScatterController,
+  BubbleController,
+  TimeScale
+} from "chart.js"
+
+// Import plugins
+import { universalImagePlugin } from "./chart-store"
+import { customLabelPlugin } from "./custom-label-plugin"
+import exportPlugin from "./export-plugin"
+
+// Register all Chart.js components globally
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  BarElement,
+  LineElement,
+  PointElement,
+  ArcElement,
+  // Controllers
+  BarController,
+  LineController,
+  PieController,
+  DoughnutController,
+  PolarAreaController,
+  RadarController,
+  Title,
+  Tooltip,
+  Legend,
+  RadialLinearScale,
+  Filler,
+  ScatterController,
+  BubbleController,
+  TimeScale,
+  universalImagePlugin,
+  customLabelPlugin,
+  exportPlugin
+)
+
+// Verify registration in development
+if (process.env.NODE_ENV === 'development') {
+  console.log('Chart.js registration completed')
+  console.log('Registered controllers:', Object.keys(ChartJS.registry.controllers))
+  console.log('Registered plugins:', Object.keys(ChartJS.registry.plugins))
+}
+
+export default ChartJS 
